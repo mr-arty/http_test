45686f9818cdd039d3478bfc156f763c67494be7
mr-arty <mr-arty@yandex.ru>
2018-07-15 09:01:05 +0000

    br2

--- search_test.py
+++ search_test.py
@@ -1,16 +1,23 @@
 import requests
 import settings
 
+def search_f(search_q, pn_q):
+    page = (pn_q - 1) * 10 + 1
+    URL = 'https://www.googleapis.com/customsearch/v1'
+    payload = {'key':settings.API_key, 'cx':settings.cx, 'q':search_q, 'start':page}
 
-URL = 'https://www.googleapis.com/customsearch/v1?key=' + settings.API_key + '&cx=' + settings.cx + '&q='
-
-def search_f(search_q):
-    r=requests.get(URL + search_q).json()
-
+    r=requests.get(URL, params = payload)
+    r=r.json()
+    
     for names in r['items']:
         print(names['title'], names['link'])
 
 print('Input search term(s): ')
 search = str(input())
+print('Input starting page: ')
+pn = int(input())
 
-search_f(search)
+while pn != 0:
+    search_f(search, pn)
+    print('Input page number: ')
+    pn = int(input())
